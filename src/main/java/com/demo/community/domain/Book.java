8efdd308576fb01cb1a02bbe7520e447eda01b5c
eohyemin <eohyemin@gmail.com>
2020-04-10 22:04:54 +0900

    JPA어노테이션, BookRepository 추가

--- src/main/java/com/demo/community/domain/Book.java
+++ src/main/java/com/demo/community/domain/Book.java
@@ -1,17 +1,27 @@
 package com.demo.community.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idx;
+
+    @Column
     private String title;
+
+    @Column
     private LocalDateTime publishedAt;
 
+    @Builder
     public Book(String title, LocalDateTime publishedAt) {
         this.title = title;
         this.publishedAt = publishedAt;
