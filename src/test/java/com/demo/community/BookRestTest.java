1ced5fa72ea799f8036718ae603df25288fd4e31
eohyemin <eohyemin@gmail.com>
2020-04-13 19:10:38 +0900

    RestClientTest - rest error테스트

--- src/test/java/com/demo/community/BookRestTest.java
+++ src/test/java/com/demo/community/BookRestTest.java
@@ -12,9 +12,11 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.HttpServerErrorException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withServerError;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @RunWith(SpringRunner.class)
@@ -37,6 +39,14 @@ public class BookRestTest {
 
         Book book = this.bookRestService.getRestBook();
         assertThat(book.getTitle()).isEqualTo("테스트");
+    }
+
+    @Test
+    public void rest_Error_테스트() {
+        this.mockServer.expect(requestTo("/rest/test"))
+                .andRespond(withServerError());
+        this.thrown.expect(HttpServerErrorException.class);
+        this.bookRestService.getRestBook();
     }
 
 }
